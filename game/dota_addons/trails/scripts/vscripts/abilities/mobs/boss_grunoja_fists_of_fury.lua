21ea4fa5c738eaa6b51f2ebd308236bc52b29743
Ethan Feinberg <sneakcow@gmail.com>
2016-05-19 05:59:54 -0700

    Added warning time on fists of fury, fixed it not ending on target death (i think)

--- game/dota_addons/trails/scripts/vscripts/abilities/mobs/boss_grunoja_fists_of_fury.lua
+++ game/dota_addons/trails/scripts/vscripts/abilities/mobs/boss_grunoja_fists_of_fury.lua
@@ -6,13 +6,22 @@ function spellCast(keys)
 	local ability = keys.ability
 	local target = keys.target
 
-	local damage_scale = ability:GetSpecialValueFor("damage_percent") / 100
+	ability:ApplyDataDrivenModifier(caster, caster, "modifier_fists_of_fury_starting", {})
+
 	local damage_to_break = ability:GetSpecialValueFor("damage_to_break")
 
 	caster.fists_of_fury_state = {}
 	caster.fists_of_fury_state.damage_to_break = damage_to_break
 	caster.fists_of_fury_state.target = target
 	caster.fists_of_fury_state.cp_reward = ability:GetSpecialValueFor("cp_reward")
+
+	caster:SetRenderColor(128, 128, 255)
+end
+
+function beginEffect(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
 
 	target:AddNewModifier(caster, ability, "modifier_faint", {duration = 0.28})
 	ability:ApplyDataDrivenModifier(caster, caster, "modifier_fists_of_fury", {})
@@ -23,18 +32,23 @@ function dealChannelDamage(keys)
 	local ability = keys.ability
 	local unit = caster.fists_of_fury_state.target
 
-	local damage_scale = ability:GetSpecialValueFor("damage_percent") / 100
-	local damage_scale_per_tick = damage_scale * 0.25 / ability:GetSpecialValueFor("duration")
-	local damage_type = ability:GetAbilityDamageType()
+	if IsValidAlive(unit) and IsValidAlive(caster) then -- i have no idea how this latter check fails but w/e
+		local damage_scale = ability:GetSpecialValueFor("damage_percent") / 100
+		local damage_scale_per_tick = damage_scale * 0.25 / ability:GetSpecialValueFor("duration")
+		local damage_type = ability:GetAbilityDamageType()
 
-	applyEffect(unit, damage_type, function()
-		dealScalingDamage(unit, caster, damage_type, damage_scale_per_tick, ability)
-		unit:AddNewModifier(caster, ability, "modifier_faint", {duration = 0.28})
-	end)
+		applyEffect(unit, damage_type, function()
+			dealScalingDamage(unit, caster, damage_type, damage_scale_per_tick, ability)
+			unit:AddNewModifier(caster, ability, "modifier_faint", {duration = 0.28})
+		end)
 
-	caster.fists_of_fury_state.cp_reward = caster.fists_of_fury_state.cp_reward - ability:GetSpecialValueFor("cp_reward") / ability:GetSpecialValueFor("duration") * 0.25
+		caster.fists_of_fury_state.cp_reward = caster.fists_of_fury_state.cp_reward - ability:GetSpecialValueFor("cp_reward") / ability:GetSpecialValueFor("duration") * 0.25
 
-	StartAnimation(caster, {duration = 0.25, activity = ACT_DOTA_ATTACK, rate = 4})
+		StartAnimation(caster, {duration = 0.25, activity = ACT_DOTA_ATTACK, rate = 4})
+	else
+		caster.fists_of_fury_state.cp_reward = 0
+		caster:RemoveModifierByName("modifier_fists_of_fury")
+	end
 end
 
 function damageTaken(keys)
@@ -54,4 +68,6 @@ function endFaint(keys)
 	caster.reward_modifier:TriggerCPReward(keys.ability:GetAbilityName(), cp)
 
 	caster.fists_of_fury_state = nil
+
+	caster:SetRenderColor(255, 255, 255)
 end
